929cfef0dd82de5afc7280df0a3ee3e625aa1c95
blankMCU <vexown@gmail.com>
2023-07-08 16:23:36 +0200

    Fix the double main functions situation

--- Software/Application/Standard/ElectronicBlinds_Main.c
+++ Software/Application/Standard/ElectronicBlinds_Main.c
@@ -51,15 +51,26 @@
 #include "ButtonTask.h"
 #include "MotorControllerTask.h"
 
+/* Hardware setup function */
+static void prvSetupHardware( void );
+
+/* Prototypes for the standard FreeRTOS callback/hook functions implemented within this file. */
+void vApplicationMallocFailedHook( void );
+void vApplicationIdleHook( void );
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+void vApplicationTickHook( void );
+
 /*----------------FUNCTION DECLARATION----------------------*/
 
-/* Main function called by main() from main.c (lol). This one does some setup and starts the scheduler */
-void ElectronicBlinds_Main( void );
 
 /*---------------GLOBAL VARIABLES DECLARATION---------------*/
 
-void ElectronicBlinds_Main( void )
+void main( void )
 {
+
+    /* Configure the Raspberry Pico hardware for blinky */
+    prvSetupHardware();
+
 	/* Create the tasks */
 	xTaskCreate( MotorControllerTask,"MotorControllerTask",configMINIMAL_STACK_SIZE,NULL,MOTOR_CONTROLLER_TASK_PRIORITY, NULL );								
 	xTaskCreate( ButtonTask, "ButtonTask", configMINIMAL_STACK_SIZE, NULL, BUTTON_TASK_PRIORITY, NULL );
@@ -76,6 +87,93 @@ void ElectronicBlinds_Main( void )
 	for( ;; );
 }
 
+/*-----------------------------------------------------------*/
+
+static void prvSetupHardware( void )
+{
+    stdio_init_all();
+
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+    gpio_put(PICO_DEFAULT_LED_PIN, 0);
+
+    gpio_init(SOURCE_3V3_1);
+    gpio_set_dir(SOURCE_3V3_1, GPIO_OUT);
+    gpio_put(SOURCE_3V3_1, 1);
+
+    gpio_init(SOURCE_3V3_2);
+    gpio_set_dir(SOURCE_3V3_2, GPIO_OUT);
+    gpio_put(SOURCE_3V3_2, 1);
+
+    gpio_init(BUTTON_UP);
+    gpio_set_dir(BUTTON_UP, GPIO_IN);
+    gpio_set_pulls(BUTTON_UP, false, true);
+
+    gpio_init(BUTTON_DOWN);
+    gpio_set_dir(BUTTON_DOWN, GPIO_IN);
+    gpio_set_pulls(BUTTON_DOWN, false, true);
+
+    gpio_init(MOTOR_CONTROL_1);
+    gpio_set_dir(MOTOR_CONTROL_1, GPIO_OUT);
+    gpio_put(MOTOR_CONTROL_1, 0);
+
+    gpio_init(MOTOR_CONTROL_2);
+    gpio_set_dir(MOTOR_CONTROL_2, GPIO_OUT);
+    gpio_put(MOTOR_CONTROL_2, 0);
+
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationMallocFailedHook( void )
+{
+    /* Called if a call to pvPortMalloc() fails because there is insufficient
+    free memory available in the FreeRTOS heap.  pvPortMalloc() is called
+    internally by FreeRTOS API functions that create tasks, queues, software
+    timers, and semaphores.  The size of the FreeRTOS heap is set by the
+    configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
+
+    /* Force an assert. */
+    configASSERT( ( volatile void * ) NULL );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
+{
+    ( void ) pcTaskName;
+    ( void ) pxTask;
+
+    /* Run time stack overflow checking is performed if
+    configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+    function is called if a stack overflow is detected. */
+
+    /* Force an assert. */
+    configASSERT( ( volatile void * ) NULL );
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationIdleHook( void )
+{
+    volatile size_t xFreeHeapSpace;
+
+    /* This is just a trivial example of an idle hook.  It is called on each
+    cycle of the idle task.  It must *NOT* attempt to block.  In this case the
+    idle task just queries the amount of FreeRTOS heap that remains.  See the
+    memory management section on the http://www.FreeRTOS.org web site for memory
+    management options.  If there is a lot of heap memory free then the
+    configTOTAL_HEAP_SIZE value in FreeRTOSConfig.h can be reduced to free up
+    RAM. */
+    xFreeHeapSpace = xPortGetFreeHeapSize();
+
+    /* Remove compiler warning about xFreeHeapSpace being set but never used. */
+    ( void ) xFreeHeapSpace;
+}
+/*-----------------------------------------------------------*/
+
+void vApplicationTickHook( void )
+{
+
+
+}
 /** 
  * 	TODO:
  * 		- Add if pressed for 2,3 sec goes all the way up/down on its own (add top/bottom edge detection first)
@@ -85,5 +183,3 @@ void ElectronicBlinds_Main( void )
  * 		- Press both buttons to activate/deactivate auto mode
  * 		- Add bluetooth control? (maybe too much lol)
 */
-
-
