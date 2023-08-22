8b83c4bfeb38797eb5b745e1436878e841b1beec
blankMCU <vexown@gmail.com>
2023-08-22 19:24:06 +0200

    Add a separate timer for debouncing limiter buttons, minor fixes

--- Software/Application/Standard/ButtonTask.c
+++ Software/Application/Standard/ButtonTask.c
@@ -28,10 +28,10 @@ static bool BottomLimitReached = false;
 SemaphoreHandle_t buttonSemaphore;
 static ButtonInfoType ButtonInfo;
 
-static void alarm_irq(void) 
+static void alarm0_InterruptHandler(void) 
 {
     /* Clear the alarm irq */
-    hw_clear_bits(&timer_hw->intr, 1u << ALARM_NUM);
+    hw_clear_bits(&timer_hw->intr, 1u << 0);
 
 	/* Debouncing delay gives the button time to stabilize its' state. The below code handles a situation when the button is released 
 	   within the debouncing delay (150ms currently) - so basically in case of very very fast button press. Without this code, in case
@@ -49,21 +49,53 @@ static void alarm_irq(void)
 	LOG("RE-ENABLE INTERRUPTS \n");
 	gpio_set_irq_enabled_with_callback(BUTTON_DOWN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &buttons_callback);
 	gpio_set_irq_enabled(BUTTON_UP, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
-	gpio_set_irq_enabled(BUTTON_TOP_LIMIT, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
+}
+
+static void alarm1_InterruptHandler(void) 
+{
+    /* Clear the alarm irq */
+    hw_clear_bits(&timer_hw->intr, 1u << 1);
+
+	/* Debouncing delay gives the button time to stabilize its' state. The below code handles a situation when the button is released 
+	   within the debouncing delay (150ms currently) - so basically in case of very very fast button press. Without this code, in case
+	   of such press, the falling edge (release of the button) would not be detected (since interrupts are disabled during debouncing 
+	   delay). This would result in the state getting stuck and not returning to STATE_OFF after button release as it should */
+    bool is_high = gpio_get(ButtonInfo.gpio);
+	if(!is_high && !(ButtonInfo.edge & GPIO_IRQ_EDGE_FALL))
+	{
+		LOG("BUTTON CHANGED TO LOW DURING DEBOUNCING DELAY - SETTING DETECTED EDGE TO FALL \n");
+		ButtonUsed = true;
+		ButtonInfo.edge = GPIO_IRQ_EDGE_FALL;
+	}
+	
+	/* Re-enable the interrupts*/
+	LOG("RE-ENABLE INTERRUPTS \n");
+	gpio_set_irq_enabled_with_callback(BUTTON_TOP_LIMIT, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &buttons_callback);
 	gpio_set_irq_enabled(BUTTON_BOTTOM_LIMIT, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true);
 }
 
-static void alarm_in_us(uint32_t delay_us) 
+static void timerInit(uint32_t delay_us, uint8_t timerNum)
 {
-    /* Enable the interrupt for the alarm */
-    hw_set_bits(&timer_hw->inte, 1u << ALARM_NUM);
-    /*  Set irq handler for alarm irq and enable it*/
-    irq_set_exclusive_handler(ALARM_IRQ, alarm_irq);
-    irq_set_enabled(ALARM_IRQ, true);
+    hw_set_bits(&timer_hw->inte, 1u << timerNum);
+
+	if(timerNum == 0)
+	{
+    	irq_set_exclusive_handler(TIMER_IRQ_0, alarm0_InterruptHandler);
+		irq_set_enabled(TIMER_IRQ_0, true);
+	}
+	else if(timerNum == 1)
+	{
+    	irq_set_exclusive_handler(TIMER_IRQ_1, alarm1_InterruptHandler);
+		irq_set_enabled(TIMER_IRQ_1, true);
+	}
+	else
+	{
+		LOG("Wrong timerNum - no handler function defined! \n");
+	}
 
 	/* Set the alarm time */
     uint64_t target = timer_hw->timerawl + delay_us;
-    timer_hw->alarm[ALARM_NUM] = (uint32_t) target;
+    timer_hw->alarm[timerNum] = (uint32_t) target;
 }
 
 void buttons_callback(uint gpio, uint32_t events)
@@ -71,17 +103,31 @@ void buttons_callback(uint gpio, uint32_t events)
 	LOG("GPIO: %d, EVENT: %d \n", gpio, events);
 	/* Disable the interrupts */ 
 	LOG("DISABLE INTERRUPTS \n");
-	gpio_set_irq_enabled_with_callback(BUTTON_DOWN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false, &buttons_callback);
-    gpio_set_irq_enabled(BUTTON_UP, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false);
-	gpio_set_irq_enabled(BUTTON_TOP_LIMIT, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false);
-	gpio_set_irq_enabled(BUTTON_BOTTOM_LIMIT, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false);
+	if((gpio == BUTTON_DOWN) || (gpio == BUTTON_UP))
+	{
+		gpio_set_irq_enabled_with_callback(BUTTON_DOWN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false, &buttons_callback);
+		gpio_set_irq_enabled(BUTTON_UP, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false);
 
+		/* Set a timer for debouncing delay - during that time interrupts are disabled - no button presses detected */
+		timerInit(DEBOUNCING_DELAY_IN_US, 0);
+	}
+	else if((gpio == BUTTON_BOTTOM_LIMIT) || (gpio == BUTTON_TOP_LIMIT))
+	{
+		gpio_set_irq_enabled_with_callback(BUTTON_TOP_LIMIT, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false, &buttons_callback);
+		gpio_set_irq_enabled(BUTTON_BOTTOM_LIMIT, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false);
+
+		/* Set a timer for debouncing delay - during that time interrupts are disabled - no button presses detected */
+		timerInit(DEBOUNCING_DELAY_IN_US, 1);
+	}
+	else
+	{
+		LOG("Button unknown - interrupts not disabled! \n");
+	}
+	
 	ButtonUsed = true;
 	ButtonInfo.gpio = gpio;
 	ButtonInfo.edge = events;
 
-	/* Set a timer for 100ms debouncing delay - during that time interrupts are disabled - no button presses detected */
-	alarm_in_us(DEBOUNCING_DELAY_IN_US);
 }
 
 void ButtonTask( void *pvParameters )
@@ -123,10 +169,12 @@ void ButtonTask( void *pvParameters )
 					break;
 				
 				case BUTTON_TOP_LIMIT:
+					LOG("CLEAR OF THE TOP LIMIT! \n");
 					TopLimitReached = false;
 					break;
 
 				case BUTTON_BOTTOM_LIMIT:
+					LOG("CLEAR OF THE BOTTOM LIMIT! \n");
 					BottomLimitReached = false;
 					break;
 				
@@ -140,20 +188,23 @@ void ButtonTask( void *pvParameters )
 			{
 				switch (ButtonInfo.gpio)
 				{
-				case BUTTON_UP:
-					if((xSemaphoreGive(buttonSemaphore) == pdTRUE) && (!TopLimitReached))
+				case BUTTON_DOWN:
+					LOG("DOWN BUTTON PRESSED! \n");
+					if((xSemaphoreGive(buttonSemaphore) == pdTRUE) && (!BottomLimitReached))
 					{
 						MotorState_Requested = STATE_CLOCKWISE;
 					}
 					break;
-				case BUTTON_DOWN:
-					if((xSemaphoreGive(buttonSemaphore) == pdTRUE) && (!BottomLimitReached))
+				case BUTTON_UP:
+					LOG("UP BUTTON PRESSED! \n");
+					if((xSemaphoreGive(buttonSemaphore) == pdTRUE) && (!TopLimitReached))
 					{
 						MotorState_Requested = STATE_ANTICLOCKWISE;
 					}
 					break;
 				
 				case BUTTON_TOP_LIMIT:
+					LOG("TOP LIMIT REACHED! \n");
 					TopLimitReached = true;
 					if(xSemaphoreGive(buttonSemaphore) == pdTRUE)
 					{
@@ -162,6 +213,7 @@ void ButtonTask( void *pvParameters )
 					break;
 
 				case BUTTON_BOTTOM_LIMIT:
+					LOG("BOTTOM LIMIT REACHED! \n");
 					BottomLimitReached = true;
 					
 					if(xSemaphoreGive(buttonSemaphore) == pdTRUE)
@@ -189,6 +241,6 @@ void ButtonTask( void *pvParameters )
  * 		- Bugfix - bottom limitter works 99% of the time, but sometimes it still allows to go down - IMPORTANT FIX
  * 				   I've notice it happens when you: go down, hit the limitter, and then very quicky press down again
  * 		- Bugfix - if you hit a limitter and cycle the power, then the system doesnt know the limit switch is pressed since it works on interrupts
+ * 		- Bugfix - sometimes the button press is detected as event 12 (both rising and falling edge), find a way to separate that 
  * 
- *  IDEA - FIGURE OUT HANDLING OF INTERRUPTS OF DOWN/UP BUTTONS AND LIMITTER AT THE SAME TIME, THIS MIGHT BE CAUSING THE ISSUE, ONE IS BLOCKING ANOTHER?
 */
