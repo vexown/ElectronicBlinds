060c34d7727b94cc493118bcff98e227a131a1aa
blankMCU <vexown@gmail.com>
2023-09-18 23:16:24 +0200

    Organize folder structure and clean up ElectronicBlinds_Main

--- Software/Application/SwComponents/Include/ElectronicBlinds_Main.h
+++ Software/Application/SwComponents/Include/ElectronicBlinds_Main.h
@@ -1,25 +1,25 @@
 #ifndef ELECTRONICBLINDS_MAIN_H
 #define ELECTRONICBLINDS_MAIN_H
 
-extern bool buttonDown_InitState, buttonUp_InitState, buttonTopLimit_InitState, buttonBottomLimit_InitState;
-
+/*--------------- MACROS ---------------*/
 /* Priorities for the tasks */
-#define MOTOR_CONTROLLER_TASK_PRIORITY		( tskIDLE_PRIORITY + 1 )
-#define	BUTTON_TASK_PRIORITY				( tskIDLE_PRIORITY + 2 )
-#define AUTOMATIC_CONTROL_TASK_PRIORITY     ( tskIDLE_PRIORITY + 3 )
+#define MOTOR_CONTROLLER_TASK_PRIORITY		(tskIDLE_PRIORITY + 1)
+#define	BUTTON_TASK_PRIORITY				(tskIDLE_PRIORITY + 2)
+#define AUTOMATIC_CONTROL_TASK_PRIORITY     (tskIDLE_PRIORITY + 3)
 
 /* Task periods (ms) */
-#define BUTTON_TASK_PERIOD						( 100 )
-#define MOTOR_CONTROLLER_TASK_PERIOD			( 100 )
-#define AUTOMATIC_CONTROL_TASK_PERIOD           ( 50000 )
+
+#define BUTTON_TASK_PERIOD					(100)
+#define MOTOR_CONTROLLER_TASK_PERIOD		(100)
+#define AUTOMATIC_CONTROL_TASK_PERIOD       (50000)
 
 /* The number of items the queue can hold */
-#define mainQUEUE_LENGTH					( 1 )
+#define mainQUEUE_LENGTH					(1)
 
 /* By default the MPU6050 devices are on bus address 0x68 */ 
 #define MPU6050_I2C_ADDRESS   				 0x68
 
-/* GPIO numbers */
+/* GPIO IDs to Names mapping: */
 #define BUTTON_UP 14U
 #define BUTTON_DOWN 15U
 #define BUTTON_TOP_LIMIT 8U
@@ -31,13 +31,13 @@ extern bool buttonDown_InitState, buttonUp_InitState, buttonTopLimit_InitState, 
 #define MOTOR_CONTROL_1 16U
 #define MOTOR_CONTROL_2 17U
 
-/* Misc */
-#define GPIO_INTERRUPTS_ENABLED 1
-#define GPIO_INTERRUPTS_DISABLED 0
-
+/* Timing macros */
 #define DEBOUNCING_DELAY_IN_US 100000U //100ms
 #define DEBOUNCING_DELAY_IN_US_LIMITTER 10000U //10ms
 #define TOP_LIMIT_SWITCH_REACTION_DURATION_IN_US 1000000 //1000ms
 #define BOTTOM_LIMIT_SWITCH_REACTION_DURATION_IN_US 200000 //200ms
+
+/*--------------- GLOBAL VARIABLES DECLARATION (extern) ---------------*/
+extern bool buttonDown_InitState, buttonUp_InitState, buttonTopLimit_InitState, buttonBottomLimit_InitState;
 
 #endif /* ELECTRONICBLINDS_MAIN_H */
