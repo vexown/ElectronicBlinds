41caa503da41823d8aa4075242707c71dd6bbc1c
blankMCU <vexown@gmail.com>
2025-10-16 22:20:45 +0200

    Refactor FreeRTOS_Kernel_import.cmake to enforce local FreeRTOS-Kernel path and remove environment variable checks

--- Software/Application/FreeRTOS_Kernel_import.cmake
+++ Software/Application/FreeRTOS_Kernel_import.cmake
@@ -5,17 +5,8 @@
 # It should be include()ed prior to project(). Alternatively this file may
 # or the CMakeLists.txt in this directory may be included or added via add_subdirectory respectively.
 
-# Set the FREERTOS_KERNEL_PATH variable if it's not yet set (or in Cache) and if it exists as a env variable:
-if (DEFINED ENV{FREERTOS_KERNEL_PATH} AND (NOT FREERTOS_KERNEL_PATH))
-    set(FREERTOS_KERNEL_PATH $ENV{FREERTOS_KERNEL_PATH})
-    message("Using FREERTOS_KERNEL_PATH from environment ('${FREERTOS_KERNEL_PATH}')")
-endif ()
-
-# if the FREERTOS_KERNEL_PATH environment variable is not set, the user could set it manually adding a path variable when running 
-# the cmake configuration command (the usual is "cmake .." but in this case it would be cmake "-DFREERTOS_KERNEL_PATH=/path/to/kernel .. ")
-# This is then cached here. CACHE option causes the variable to be stored in the CMake cache. 
-# The CMake cache is a file that stores the values of CMake variables so that they can be reused between runs of CMake.
-set(FREERTOS_KERNEL_PATH "${FREERTOS_KERNEL_PATH}" CACHE PATH "Path to the FREERTOS Kernel" FORCE)
+# Force FREERTOS_KERNEL_PATH to point to the local FreeRTOS-Kernel submodule
+set(FREERTOS_KERNEL_PATH "${CMAKE_CURRENT_LIST_DIR}/../FreeRTOS-Kernel" CACHE PATH "Path to the FREERTOS Kernel" FORCE)
 
 get_filename_component(FREERTOS_KERNEL_PATH "${FREERTOS_KERNEL_PATH}" REALPATH BASE_DIR "${CMAKE_BINARY_DIR}")
 
